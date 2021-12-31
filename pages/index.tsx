06d6551cb4ea473da41cdee088f0c5fe2ad5587c
user <user@usernoAir-2.AirPort>
2021-12-31 16:04:35 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+// pages/index.js
+import Link from "next/link";
+import { FC } from "react";
+import { client } from "../libs/client";
+import { Blog } from "../types";
+
+type HomeProps = { blogs: Blog[] };
+
+const Home: FC<HomeProps> = ({ blogs }) => {
+  return (
+    <div>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  };
+};
+
+export default Home;
