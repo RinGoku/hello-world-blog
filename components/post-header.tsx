9fed3219fece4af97e255acde42e7f7082bf8159
RinGoku <yuyu80105@gmail.com>
2021-12-31 19:25:13 +0900

    fix to lint

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -7,34 +7,40 @@ import { getBackgroundClass, getTextClass } from "../helpers/theme";
 import { getFormattedDate } from "../helpers/date";
 
 type PostHeaderProps = {
-    post: PostMetadata;
+  post: PostMetadata;
 };
 
 const PostHeader: FunctionComponent<PostHeaderProps> = (props) => {
-    const { post } = props;
+  const { post } = props;
 
-    return (
-        <header>
-            <time className="text-cool-gray-600 dark:text-cool-gray-300 flex items-center">
-                <FontAwesomeIcon
-                    className={`mr-2 h-4 ${getTextClass(post.theme)}`}
-                    icon={faClock}
-                ></FontAwesomeIcon>
-                {getFormattedDate(post.date)}
-            </time>
-            <h1 className="text-2xl md:text-3xl lg:text-4xl mb-6 dark:text-cool-gray-100">{post.title}</h1>
-            {/* POST TAGS */}
-            <div className="flex items-center">
-                <FontAwesomeIcon
-                    className={`mr-2 h-4 ${getTextClass(post.theme)}`}
-                    icon={faTag}
-                ></FontAwesomeIcon>
-                {post.tags && <TagList tags={post.tags}></TagList>}
-            </div>
-            {/* SEPARATOR */}
-            <div className={`${getBackgroundClass(post.theme)} mx-auto rounded h-2 mt-4 mb-8`}></div>
-        </header>
-    );
+  return (
+    <header>
+      <time className="text-cool-gray-600 dark:text-cool-gray-300 flex items-center">
+        <FontAwesomeIcon
+          className={`mr-2 h-4 ${getTextClass(post.theme)}`}
+          icon={faClock}
+        ></FontAwesomeIcon>
+        {getFormattedDate(post.publishedAt)}
+      </time>
+      <h1 className="text-2xl md:text-3xl lg:text-4xl mb-6 dark:text-cool-gray-100">
+        {post.title}
+      </h1>
+      {/* POST TAGS */}
+      <div className="flex items-center">
+        <FontAwesomeIcon
+          className={`mr-2 h-4 ${getTextClass(post.theme)}`}
+          icon={faTag}
+        ></FontAwesomeIcon>
+        {post.tags && <TagList tags={post.tags}></TagList>}
+      </div>
+      {/* SEPARATOR */}
+      <div
+        className={`${getBackgroundClass(
+          post.theme
+        )} mx-auto rounded h-2 mt-4 mb-8`}
+      ></div>
+    </header>
+  );
 };
 
 export default PostHeader;
